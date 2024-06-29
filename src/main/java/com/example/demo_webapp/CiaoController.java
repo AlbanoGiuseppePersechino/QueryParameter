94bd8eab680e56592c6a11dd42cdcda0fcc5f3e8
AlbanoGiuseppePersechino <albano.persechino@gmail.com>
2024-06-29 13:47:04 +0200

    Query Parameters

--- src/main/java/com/example/demo_webapp/CiaoController.java
+++ src/main/java/com/example/demo_webapp/CiaoController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/v1")
 
-public class HelloWorldController {
+public class CiaoController {
 
     @GetMapping(path = "/ciao")
     public String ciao (@RequestParam String nome, @RequestParam String provincia) {
